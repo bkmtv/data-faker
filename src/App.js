1974f6cb12d21ca68d2efa877b3bc728822f326c
Yuri Bekmetov <bekm.yu@yandex.ru>
2023-01-27 17:00:09 +0500

    added list of users

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import './App.css';
-import Users from "./components/Users";
+import { faker } from "@faker-js/faker";
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function App() {
+  faker.setLocale("ru");
+
+  let listOfUsers = [];
+
+  function createUser() {
+    return {
+      uuid: faker.datatype.uuid(),
+      fullName: faker.name.fullName(),
+      address: faker.address.streetAddress(true),
+      phone:faker.phone.number(),
+    }
+  }
+
+  Array.from({ length: 10 }).forEach(() => {
+    listOfUsers.push(createUser());
+  });
+
+  const fetchUsers = () => {
+    console.log("fetchUsers");
+    listOfUsers.push(
+      Array.from({ length: 10 }).forEach(() => {
+        listOfUsers.push(createUser());
+      })
+    )
+  };
+
   return (
     <div className="App">
       <h3>Случайные данные</h3>
-      <Users />
+      
+      <InfiniteScroll
+        dataLength={listOfUsers.length}
+        next={fetchUsers}
+        hasMore={true}
+      >
+        {listOfUsers.map((el, key) => {
+          return (
+            <div key={key} className="card">
+              <p>Index: {key + 1}</p>
+              <p>ID: {el.uuid}</p>
+              <p>Full Name: {el.fullName}</p>
+              <p>Address: {el.address}</p>
+              <p>Phone: {el.phone}</p>
+            </div>
+            )
+          })
+        }
+      </InfiniteScroll>
     </div>
   );
 }
