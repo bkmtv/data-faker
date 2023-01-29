4848c47b1659d9a85fb701fab096e99a05225a7f
Yuri Bekmetov <bekm.yu@yandex.ru>
2023-01-30 04:06:28 +0500

    small fixes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import './App.css';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import { useState, useEffect } from "react";
 import { faker } from "@faker-js/faker";
 
 function App() {
   faker.setLocale("en");
+  
   const [listOfUsers, setListOfUsers] = useState([]);
-
+  
   function createUser() {
     return {
       uuid: faker.datatype.uuid(),
       fullName: faker.name.fullName(),
       address: faker.address.streetAddress(true),
-      phone:faker.phone.number(),
-    }
+      phone: faker.phone.number(),
+    };
   }
   const newUsers = [];
   Array.from({ length: 10 }).forEach(() => {
@@ -28,37 +29,41 @@ function App() {
   }, []);
 
   const scrollHandler = (e) => {
-      if ((e.target.documentElement.scrollHeight - e.target.documentElement.scrollTop - window.innerHeight) < 100) {
-        setListOfUsers([...newUsers, ...newUsers]);
-      }
+    if (
+      e.target.documentElement.scrollHeight -
+        e.target.documentElement.scrollTop -
+        window.innerHeight <
+      100
+    ) {
+      console.log("render");
+      setListOfUsers([...newUsers, ...newUsers]);
     }
+  };
 
   useEffect(() => {
     document.addEventListener("scroll", scrollHandler);
     return () => {
       document.removeEventListener("scroll", scrollHandler);
-    }
+    };
   }, []);
-
 
   return (
     <div className="App">
       <h2>Fake Users</h2>
       <button onClick={refresh}>Refresh</button>
-        {listOfUsers.map((el, key) => {
-          return (
-            <div key={key} className="card">
-              <p>Index: {key + 1}</p>
-              <p>ID: {el.uuid}</p>
-              <p>Full Name: {el.fullName}</p>
-              <p>Address: {el.address}</p>
-              <p>Phone: {el.phone}</p>
-            </div>
-            )
-          })
-        }
+      {listOfUsers.map((el, key) => {
+        return (
+          <div key={key} className="card">
+            <p>Index: {key + 1}</p>
+            <p>ID: {el.uuid}</p>
+            <p>Full Name: {el.fullName}</p>
+            <p>Address: {el.address}</p>
+            <p>Phone: {el.phone}</p>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
 export default App;
